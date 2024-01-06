7f407b3918d470f1abe6dd28e284e01b4670cca4
Sauhard Sharma <sauhard.sherlockholmes@gmail.com>
2024-01-06 00:23:28 -0800

    Added basic search functionality based on an API
    Used states and effects to display results on the spot

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [searchval, setSearchval] = useState();
+  const [searchResult, setSearchResult] = useState();
+  const [display, setDisplay] = useState();
+
+  const setText = (input) => {
+    console.log("Current Input received: "+input)
+    setSearchval(input)
+    handleSubmit(input)
+  }
+
+  const handleSubmit = (input) => {
+    console.log("Search: "+input)
+    fetch(`https://api.mfapi.in/mf/search?q=${input}`)
+      .then((response) => response.json())
+      .then((data) =>  {
+        console.log("API resp data: "+data)
+        setSearchResult(data)        
+      })
+    
+  };
+  useEffect(() => {
+    console.log("SearchResults "+searchResult)
+    const displayDivs = () => {
+      if (searchResult === undefined) return
+      console.log(searchResult);
+      const jobList = searchResult.map(j =>
+        <div>
+          <div>{j.schemeName}</div>
+        </div>
+      )
+      return (
+        <div>{jobList}</div>
+      );
+    }
+    setDisplay(displayDivs())
+  }, [searchResult]);
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Search Mutual Funds 
+        
+        <br></br>
+        <input 
+          type="text"
+          id="mfsearch"
+          class="actions"
+          onChange={(event) => setText(event.target.value)}
+        ></input>
+        <br></br>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +62,9 @@ function App() {
         >
           Learn React
         </a>
+        <br></br>
+        {display}
+        
       </header>
     </div>
   );
